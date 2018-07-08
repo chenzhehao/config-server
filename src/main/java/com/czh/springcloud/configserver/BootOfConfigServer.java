5df1c82ac2c8646ceaa6fc566488383899c92d1b
orign <chenzhehao2008666@icloud.com>
2018-07-08 15:48:58 +0800

    增加手风琴菜单

---
+++ src/main/java/com/czh/springcloud/configserver/BootOfConfigServer.java
@@ -0,0 +1,29 @@
+package com.czh.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Title: BootOfRocketMq.java Description: Copyright: Copyright (c) 2018
+ * Company: www.chenzhehao.com
+ * 
+ * @author chenzhehao
+ * @date 2018年5月24日
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+public class BootOfConfigServer {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BootOfConfigServer.class, args);
+	}
+
+	@RequestMapping("test")
+	public Object test(){
+		return "test";
+	}
+}
