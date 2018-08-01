a88905194f54ce60cd6149c3f7a2d0400f2a0410
orign <chenzhehao2008666@icloud.com>
2018-08-01 10:00:12 +0800

    提取eureka注册jar包到root中，端口分配规则修改

--- src/main/java/com/czh/cloud/configserver/BootOfConfigServer.java
+++ src/main/java/com/czh/cloud/configserver/BootOfConfigServer.java
@@ -2,6 +2,7 @@ package com.czh.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableConfigServer //添加@EnableConfigServer注解，表明这是一个Config Server端
 @RestController
+@EnableDiscoveryClient
 public class BootOfConfigServer {
 
     public static void main(String[] args) throws Exception {
