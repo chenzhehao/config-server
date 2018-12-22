5371f58529f09d8620dfc2904bb033879b55c143
chenzhehao <chenzhehao2008666@icloud.com>
2018-12-22 23:52:24 +0800

    服务增加测试接口，配置修改，解决引入web报错hibernate问题，jboss jar包问题

--- src/main/java/com/czh/cloud/configserver/BootOfConfigServer.java
+++ src/main/java/com/czh/cloud/configserver/BootOfConfigServer.java
@@ -33,8 +33,8 @@ public class BootOfConfigServer {
         SpringApplication.run(BootOfConfigServer.class, args);
     }
 
-    @RequestMapping("/test")
+    @RequestMapping("/")
     public Object test() {
-        return "test";
+        return "success";
     }
 }
